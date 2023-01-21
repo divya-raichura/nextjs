e430b5997d2bf99d464c1944ffd5659bb958564a
Divya Raichura <divyaraichura8@gmail.com>
2023-01-21 13:47:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,8 +45,8 @@ export default function Home({ allPostsData }) {
           <a href="https://twitter.com/divya_raichura">Twitter</a>
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website that I built using {" "}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
